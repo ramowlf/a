43889594f330cf4bbc476789a30d50e0e87a4efb
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2023-09-18 15:46:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_OWNER = os.environ.get("BOT_OWNER") 
+    API_ID = int(os.environ.get("API_ID", "25114508"))
+    API_HASH = os.environ.get("API_HASH", "993ccdfe81548dade420e81bcd6807ce")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
