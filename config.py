f51213ef559c7b9a0117e5203a8272923e5a3773
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2023-09-26 15:42:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "25114508"))
-    API_HASH = os.environ.get("API_HASH", "993ccdfe81548dade420e81bcd6807ce")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_OWNER = os.environ.get("BOT_OWNER", "")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    BOT_CHANNEL = oe.environ.get("BOT_CHANNEL", "")
+    API_ID = int(os.environ.get("API_ID", "25114508")) #Karışmayın
+    API_HASH = os.environ.get("API_HASH", "993ccdfe81548dade420e81bcd6807ce") #Karışmayın
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #Botunuzun Tokenini Girin .
+    BOT_OWNER = os.environ.get("BOT_OWNER", "") #Bot Sahibi Kullanıcı Adı Girin .
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "") #Bot Kullanıcı Adı Girin .
+    BOT_CHANNEL = os.environ.get("BOT_CHANNEL", "") #Kanal Kullanıcı Adı Girin .
     
