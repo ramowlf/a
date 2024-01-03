d36f38a72a9974099432a53077547756c88f6da7
Denizehewq <155530851+Denizehewq@users.noreply.github.com>
2024-01-03 15:54:10 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -9,7 +9,7 @@ bot = Client(
 )
 
 # MERHABA KOMUTU
-@bot.on_message(filters.text)
+@bot.on_message(filters.text & ~filters.command)
 def merhaba_command(client, message):
     if "merhaba" in message.text.lower():
         message.reply_text("Merhaba!")
@@ -17,15 +17,17 @@ def merhaba_command(client, message):
 # START KOMUTU
 @bot.on_message(filters.command(["start"]))
 def start_command(client, message):
-    helptext = f'KEY Botudur Aşağıdaki Kanala katilmadiginiz tespit edildiği halde banyiyecksin'
+    helptext = f'**📥 Telegram Müzik & Video İndirme Botudur, Tamamen Ücretsizdir ...\n\n» /bul < müzik adı >\n    - Anında Müzik İndirir ...\n» /vbul < video adı >\n    - Anında Video İndirir ...**'
     message.reply_text(
         text=helptext,
         quote=False,
         reply_markup=InlineKeyboardMarkup(
-         [[
+            [[
+                InlineKeyboardButton('💌 ʙᴇɴɪ ɢʀᴜʙᴀ ᴇᴋʟᴇ 💌', url=f'http://t.me/DenizzmusiccBot?startgroup=new'),
+            ], [
                 InlineKeyboardButton('📚 ᴋᴀɴᴀʟ', url=f'https://t.me/japonicd')
             ], [
-                InlineKeyboardButton('👤 ᴏᴡɴᴇʀ', url=f'https://t.me/sakultahbey')
+                InlineKeyboardButton('👤 ᴏᴡɴᴇʀ', url=f'https://t.me/sakultah')
             ]
             ]
         )
