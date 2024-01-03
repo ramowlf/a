2d8a1f8cef38f78f341769ec33d9094a79b23872
Denizehewq <155530851+Denizehewq@users.noreply.github.com>
2024-01-03 16:18:32 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -1,3 +1,5 @@
+import requests
+
 from pyrogram.handlers import MessageHandler
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -11,6 +13,15 @@ bot = Client(
     api_hash=Config.API_HASH
 )
 
+# Function to get content from a text file on a website
+def get_key_from_website(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+        return response.text
+    except requests.exceptions.RequestException as e:
+        return f"Failed to fetch key. Error: {e}"
+
 # START KOMUTU
 @bot.on_message(filters.command(["start"]))
 def start_command(client, message):
@@ -20,30 +31,19 @@ def start_command(client, message):
         reply_markup=InlineKeyboardMarkup(
             [[
                 InlineKeyboardButton('📚 ᴋᴀɴᴀʟ', url=f'https://t.me/japonicd')
-              ] 
-                              
-            ]
+              ]] 
         )
     )
 
 # Key KOMUTU
 @bot.on_message(filters.command(["key"]))
-def start_command(client, message):
+def key_command(client, message):
+    website_url = 'http://sakultah.fun/a.txt'  # Replace with your actual URL
+    key_content = get_key_from_website(website_url)
+
     bot.send_message(
         chat_id=message.chat.id,
-        text="AŞAĞIDAKİ BUTTONDAN KEY ALABİLİRSİNİZ",
-        reply_markup=InlineKeyboardMarkup(
-            [[
-                InlineKeyboardButton('📚 ᴋᴀɴᴀʟ', url=f'http://sakultah.fun/index.php')
-              ] 
-                              
-            ]
-        )
+        text=key_content
     )
-
-
-
-
-
 
 bot.run()
