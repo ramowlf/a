c59b6a817eee9c44c4271ebea96dd3b9d89a62e8
Denizehewq <155530851+Denizehewq@users.noreply.github.com>
2024-01-03 14:15:46 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 
 bot = Client(
     'moonBot',
-    bot_token='YOUR_BOT_TOKEN',
-    api_id='YOUR_API_ID',
-    api_hash='YOUR_API_HASH'
+    bot_token='6756702590:AAEgJpC7qdmbg-5dYwpWLax0FqAh6bULkRc',
+    api_id='23664317',
+    api_hash='8c246b2d2b2455ff7bef02ae0178eefa'
 )
 
 # MERHABA KOMUTU
