199e721d53e4ad657c7715722b50c2025fc8454f
Denizehewq <155530851+Denizehewq@users.noreply.github.com>
2024-01-03 14:14:06 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    API_ID = int(os.environ.get("API_ID", "23664317")) #Karışmayın
-    API_HASH = os.environ.get("API_HASH", "8c246b2d2b2455ff7bef02ae0178eefa") #Karışmayın
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6756702590:AAEgJpC7qdmbg-5dYwpWLax0FqAh6bULkRc") #Botunuzun Tokenini Girin .  
