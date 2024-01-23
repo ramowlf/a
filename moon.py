e9b7056abf0260085817ff29f43af079a38e3eaa
Denizehewq <155530851+Denizehewq@users.noreply.github.com>
2024-01-23 12:23:40 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -9,34 +9,29 @@ bot = Client(
     api_hash=Config.API_HASH
 )
 
-allowed_user_id = 6698881784  # Sadece belirli bir kullanıcının dosya yüklemesine izin verecek ID'dir
+allowed_user_id = 6698881784  # Bu, sadece belirli bir kullanıcının dosya yüklemesine izin verecek ID'dir
 
-def upload_file(message, upload_url):
+@bot.on_message(filters.document & filters.private)
+def upload_document(client, message):
     if message.from_user.id != allowed_user_id:
         bot.send_message(
             chat_id=message.chat.id,
-            text="Üzgünüm, dosya yüklemek için izniniz yok."
+            text="SİKTİR GİT YARAM SAKULTAH SİKER ANANI İKİLE "
         )
         return
 
-    if not message.document:
-        bot.send_message(
-            chat_id=message.chat.id,
-            text="Lütfen yüklemek istediğiniz belgeyi ekleyin."
-        )
-        return
+    file_id = message.document.file_id
+    file_name = message.document.file_name  # Dosyanın orijinal adını al
 
-    bot.send_message(
-        chat_id=message.chat.id,
-        text="Dosya yükleme işlemi başlatılıyor..."
-    )
+    file_path = client.download_media(message, file_name='downloads/' + file_name)
+
+    upload_url = "https://sngvip.fun/upload.php"
+    files = {'file': (file_name, open(file_path, 'rb'))}  # Dosyanın adını kullan
 
     try:
-        file_name = message.document.file_name
-        file_path = bot.download_media(message, file_name='downloads/' + file_name)
-        files = {'file': (file_name, open(file_path, 'rb'))}
-
         response = requests.post(upload_url, files=files)
+
+        # If upload is successful
         if response.status_code == 200:
             bot.send_message(
                 chat_id=message.chat.id,
@@ -54,16 +49,14 @@ def upload_file(message, upload_url):
             text="Dosya yüklenirken bir hata oluştu."
         )
 
-@bot.on_message(filters.command("upload") & filters.private)
+@bot.on_message(filters.command("upload"))
 def trigger_upload(client, message):
-    upload_url = "https://sngvip.fun/upload.php"
-    upload_file(message, upload_url)
-
-@bot.on_message(filters.command("upload2") & filters.private)
-def trigger_upload2(client, message):
-    upload_url = "https://sngvip.fun/upload2.php"
-    upload_file(message, upload_url)
+    # Add any conditions or additional checks here if needed
+    # Trigger the upload process by sending a document
+    bot.send_message(
+        chat_id=message.chat.id,
+        text="Dosya yüklemek için bir belge gönderin."
+    )
 
 # Bot'u başlat
 bot.run()
-        
