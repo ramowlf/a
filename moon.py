3bccb938f1b7e72c5e8a6cf6a72981ca59af8124
Denizehewq <155530851+Denizehewq@users.noreply.github.com>
2024-05-18 00:56:44 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -1,75 +1,148 @@
-import telebot
-import requests
-from datetime import datetime, timedelta
+import wget
+import os, youtube_dl, requests, time
 
-TOKEN = '7012512707:AAFc3gLNJdzxvmNfRv7cqAjqlCInSy_9qEE'
+from youtube_search import YoutubeSearch
 
-bot = telebot.TeleBot(TOKEN)
+from pyrogram.handlers import MessageHandler
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery 
+from yt_dlp import YoutubeDL
+import os, youtube_dl, requests, time
+from config import Config
+from youtube_search import YoutubeSearch
+from pyrogram.handlers import MessageHandler
+from pyrogram import Client, filters
+import yt_dlp
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message
+)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    user_id = message.from_user.id
-    first_name = message.from_user.first_name
-    
-    welcome_message = f"*TSG Key Botuna Hoşgeldin {first_name} \n/key yazarak keyini alabilirsin.\n\n! Spam yaparsan bottan banlanırsın.*"
-    
-    bot.reply_to(message, welcome_message, parse_mode="Markdown")
 
-def is_user_in_channel(chat_id, channel_username):
+bot = Client(
+    'moonBot',
+    bot_token = Config.BOT_TOKEN,
+    api_id = Config.API_ID,
+    api_hash = Config.API_HASH
+)
+
+# START - HELP KOMUTU
+@bot.on_message(filters.command(["start", "help"]))
+def help(client, message):
+    helptext = f'**📥 Telegram Müzik & Video İndirme Botudur, Tamamen Ücretsizdir ...\n\n» /bul < müzik adı >\n    - Anında Müzik İndirir ...\n» /vbul < video adı >\n    - Anında Video İndirir ...**'
+    message.reply_text(
+        text=helptext, 
+        quote=False,
+        reply_markup=InlineKeyboardMarkup(
+            [[
+                    InlineKeyboardButton('💌 ʙᴇɴɪ ɢʀᴜʙᴀ ᴇᴋʟᴇ 💌', url=f'http://t.me/{BOT_USERNAME}?startgroup=new'),
+                  ],[
+                    InlineKeyboardButton('📚 ᴋᴀɴᴀʟ', url=f'https://t.me/{BOT_CHANNEL}')
+                  ],[
+                    InlineKeyboardButton('👤 ᴏᴡɴᴇʀ', url=f'https://t.me/{BOT_OWNER}')
+                  ]
+            ]
+        )
+    )
+
+# MÜZİK İNDİRME KOMUTU
+@bot.on_message(filters.command(["bul", "song"]) & ~filters.edited)
+async def bul(_, message):
+    query = " ".join(message.command[1:])
+    m = await message.reply("➻ **sᴀʀᴋɪ ᴀʀᴀɴɪʏᴏʀ ...**")
+    ydl_ops = {"format": "bestaudio[ext=m4a]"}
     try:
-        chat_member = bot.get_chat_member(channel_username, chat_id)
-        return chat_member.status != "left"
-    except telebot.apihelper.ApiException as e:
-        print(f"API Hatası: {e}")
-        return False
-
-last_key_time = {}
-
-def is_user_allowed_to_get_key(user_id):
-    if user_id in last_key_time:
-        last_retrieval_time = last_key_time[user_id]
-        time_since_last_retrieval = datetime.now() - last_retrieval_time
-        
-        if time_since_last_retrieval < timedelta(hours=24):
-            return False
-    return True
-
-@bot.message_handler(commands=['key'])
-def send_key(message):
-    try:
-        user_id = message.from_user.id
-        
-        channel_username = '@TSGxMODS'
-        if not is_user_in_channel(user_id, channel_username):
-            bot.reply_to(message, "Anahtar alabilmek için önce @TSGxMODS kanalımıza katılmanız gerekmektedir.", parse_mode="Markdown")
-            return
-               
-        if not is_user_allowed_to_get_key(user_id):
-            bot.reply_to(message, "24 saat içinde bir kez anahtar alabilirsiniz.")
-            return
-
-        # Fetch the key from the external URL
-        response = requests.get("https://modakaidem.online/2SyAmh0ND7ZMKjPZhi.php")
-        if response.status_code == 200:
-            user_key = response.text.strip()
-        else:
-            bot.reply_to(message, "Anahtar alınırken bir hata oluştu.")
-            return
-
-        last_key_time[user_id] = datetime.now()
-
-        bot.reply_to(message, f"Anahtarınız: `{user_key}`", parse_mode="MarkdownV2")
-        
-        log_message = f"Yeni Anahtar Atıldı!\n" \
-                      f"Anahtar: {user_key}\n" \
-                      f"Atayan ID: {message.from_user.id}\n" \
-                      f"Atayan Adı: {message.from_user.first_name}\n" \
-                      f"Atayan K. Adı: @{message.from_user.username}\n" \
-                      f"Tarih ve Saat: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-        bot.send_message(-1002079991334, log_message)  
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
 
     except Exception as e:
-        bot.reply_to(message, f"Hata oluştu: {e}")
+        await m.edit("➻ **şᴀʀᴋɪ ʙᴜʟᴜɴᴀᴍᴀᴅɪ ...**")
+        print(str(e))
+        return
+    await m.edit("➻ **şᴀʀᴋɪ ɪɴᴅɪʀɪʟɪʏᴏʀ ...**")
+    try:
+        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f"**➻ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n➻ sᴜ̈ʀᴇ : {duration}\n\n➻ ɪsᴛᴇʏᴇɴ : {message.from_user.first_name}**"
+        res = f"**➻ ᴘᴀʀᴄ̧ᴀ : {title[:35]}\n➻ sᴜ̈ʀᴇ : {duration}\n\n➻ ɪsᴛᴇʏᴇɴ : {message.from_user.first_name}**"
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(float(dur_arr[i])) * secmul
+            secmul *= 60
+        await m.edit("➻ **şᴀʀᴋɪ ʏᴜ̈ᴋʟᴇɴɪʏᴏʀ ...**")
+        await message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name, performer="♫︎ 𝐌𝐮̈𝐳𝐢𝐤 𝐈𝐧𝐝𝐢𝐫𝐢𝐜𝐢 ♫︎")
+        await m.delete()
+        await _.send_audio(chat_id=PLAYLIST_ID, audio=audio_file, caption=res, performer="♫︎ 𝐌𝐮̈𝐳𝐢𝐤 𝐈𝐧𝐝𝐢𝐫𝐢𝐜𝐢 ♫︎", parse_mode='md', title=title, duration=dur, thumb=thumb_name)
+    except Exception as e:
+        await m.edit("🔺 **ʙᴇɴɪ ʏᴏɴᴇᴛɪᴄɪ ʏᴀᴘɪɴ ...**")
+        print(e)
 
-bot.polling()
-            
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
+
+
+# VİDEO İNDİRME KOMUTU
+@bot.on_message(
+    filters.command(["vbul", "vsong"]) & ~filters.edited
+)
+async def vsong(client, message):
+    ydl_opts = {
+        "format": "best",
+        "keepvideo": True,
+        "prefer_ffmpeg": False,
+        "geo_bypass": True,
+        "outtmpl": "%(title)s.%(ext)s",
+        "quite": True,
+    }
+    query = " ".join(message.command[1:])
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        results[0]["duration"]
+        results[0]["url_suffix"]
+        results[0]["views"]
+        message.from_user.mention
+    except Exception as e:
+        print(e)
+    try:
+        msg = await message.reply("➻ **ᴠɪᴅᴇᴏ ᴀʀᴀɴɪʏᴏʀ ...**")
+        with YoutubeDL(ydl_opts) as ytdl:
+            ytdl_data = ytdl.extract_info(link, download=True)
+            file_name = ytdl.prepare_filename(ytdl_data)
+    except Exception as e:
+        return await msg.edit(f"➻ **ᴠɪᴅᴇᴏ ʙᴜʟᴜɴᴀᴍᴀᴅɪ ...**")
+    preview = wget.download(thumbnail)
+    await msg.edit("➻ **ᴠɪᴅᴇᴏ ɪɴᴅɪʀɪʟɪʏᴏʀ ...**")
+    await message.reply_video(
+        file_name,
+        duration=int(ytdl_data["duration"]),
+        thumb=preview,
+        caption=ytdl_data["title"],
+    )
+    try:
+        os.remove(file_name)
+        await msg.delete()
+    except Exception as e:
+        print(e)
+
+
+
+bot.run()
+    
