edf859b4b18cd2f91a2d725b39b8d330ae0d90a1
ramowlf <ramodiscord26@gmail.com>
2024-07-05 11:09:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "23664317")) #Karışmayın
-    API_HASH = os.environ.get("API_HASH", "8c246b2d2b2455ff7bef02ae0178eefa") #Karışmayın
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6828936442:AAEzw1W9uZoyPHMCMcGIzQEWhJp0x8meubQ") #Botunuzun Tokenini Girin .  
+    API_ID = int(os.environ.get("API_ID", "16088758")) #Karışmayın
+    API_HASH = os.environ.get("API_HASH", "7c959970fc76db9846339b79b7bd8aae") #Karışmayın
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7467806086:AAGGOiF1iGxiBJKKJIiU3tz4rkdEoEVMqS8") #Botunuzun Tokenini Girin .  
 
